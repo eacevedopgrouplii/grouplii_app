96a71ac438a1f337898065b50b8576f736423c7c
edwin.acevedo <edwin.acevedo@sophossolutions.com>
2022-04-19 15:40:08 -0500

    change requirements

--- grouplii_backend_dev/sql_app/database.py
+++ grouplii_backend_dev/sql_app/database.py
@@ -1,8 +1,24 @@
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:groupliiroot@localhost:3306/grouplii"
+
+#SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:groupliiroot@localhost:3306/grouplii"
+
+SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:db_groupliitest2022@localhost:3306/grouplii?unix_socket=/cloudsql/groupliitest:us-east1:dbgroupliitest"
+
+'''
+SQLALCHEMY_DATABASE_URL = sqlalchemy.engine.url.URL.create(
+            drivername="mysql+pymysql",
+            username="root",  # e.g. "my-database-user"
+            password="db_groupliitest2022",  # e.g. "my-database-password"                        
+            database="grouplii",  # e.g. "my-database-name"
+            query={
+            "unix_socket": "/cloudsql/groupliitest:us-east1:dbgroupliitest"
+            }
+        ),
+'''
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
